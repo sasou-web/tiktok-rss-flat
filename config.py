259702562e07c24efb3064317687ee950771e8fb
sasou-web <sasou-web@users.noreply.github.com>
2026-06-08 18:00:56 +0200

    Configure fork: GH Pages URL + abonnement kayagoldforged

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
-ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
-
-# ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://sasou-web.github.io/tiktok-rss-flat/"
+ghRawURL = "https://raw.githubusercontent.com/sasou-web/tiktok-rss-flat/main/"
